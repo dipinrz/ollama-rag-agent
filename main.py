dba750ea58a76cdc5168a8b340d1602724781b98
dipinrz <dipindas.ah@reizend.ai>
2025-09-09 18:14:20 +0530

    update the codeusing the gemin ai llms

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from langchain_ollama.llms import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 from vector import retriver
-model= OllamaLLM(model="llama3.2")
+import os
+os.environ["GOOGLE_API_KEY"] = "AIzaSyA8bLpkN-jw81TVXW3-n3spPx0nz-F0bB0"
+
+# model= OllamaLLM(model="llama3.2")
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
 template= """
 you are an expert in answering questions about the pizza restaurant
 here are some relevent reviews :{reviews}
